f49013f2ab7f76eba2de998b6e2dc6dec55be518
Rohit Goswami <lynus.mascot@gmail.com>
2018-07-19 19:14:39 +0530

    Create Patten_1.java

---
+++ Patten_1.java
@@ -0,0 +1,33 @@
+//Author: Rohit Goswami , lynus.mascot@gmil.com
+
+/* 
+input : a[5]= {3,2,4,5,1}
+
+output: 1:          *
+        2:       *  *
+        3: *     *  *
+        4: *  *  *  *
+        5: *  *  *  *  *
+*/
+public class Main{
+    public static void main(String args[]) {
+int[] a = new int[]{3,2,4,5,1};
+    char[][] ab = new char[10][10];
+    int i,k,j,l;
+      for(j=0;j<5;j++)
+        {
+        for(l=5-a[j];l<5;l++)
+          {ab[l][j]='*';}
+          
+    }
+    for( i = 0; i<5; i++)
+    {
+        for( j = 0; j<5; j++)
+        {
+            System.out.print(ab[i][j]);
+            System.out.print("");
+        }
+            System.out.println();
+}
+}
+    }
